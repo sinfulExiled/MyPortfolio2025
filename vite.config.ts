fcfcedfa994de870f58ac1c398ecde9aee3d6f65
ashain siriwardane <sinfulexiled@gmail.com>
2025-09-03 21:12:10 +0530

    Fix vite base config for local and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/MyPortfolio2025/",
-});
+  base: command === "serve" ? "/" : "/MyPortfolio2025/",
+}));
