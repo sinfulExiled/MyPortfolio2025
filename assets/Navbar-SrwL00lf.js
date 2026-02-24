c5b8cd3f9c53cb29b0131c99ee93ee4bc887a49d
ashain siriwardane <sinfulexiled@gmail.com>
2026-02-24 13:31:52 +0530

    Updates

--- assets/Navbar-SrwL00lf.js
+++ assets/Navbar-SrwL00lf.js
@@ -1,4 +1,4 @@
-import{j,r as vt}from"./index-n_SPP4QG.js";import{g as wt,S as bt}from"./ScrollTrigger-CUwdeFQJ.js";/*!
+import{j,r as vt}from"./index-DaLMOhTC.js";import{g as wt,S as bt}from"./ScrollTrigger-CUwdeFQJ.js";/*!
  * SplitText 3.13.0
  * https://gsap.com
  *
